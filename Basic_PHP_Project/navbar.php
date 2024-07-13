182d1f599fbe4c59c186e4eadf8f62b491223d14
jak_shaon <106922308+jak-Shaon@users.noreply.github.com>
2024-07-13 14:21:24 +0600

    Add files via upload

---
+++ Basic_PHP_Project/navbar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg bg-body-dark pt-2.5">
+  <div class="container-fluid">
+
+    <h2><a class="navbar-brand" href="home.php">Jak Shaon</a></h2>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <h5><a class="nav-link active" aria-current="page" href="dashboard.php">Home</a></h5>
+      </li>
+    </ul>
+    <form class="d-flex">
+      <a class="btn btn-outline-success mx-2" type="submit" href="signup.php">Signup</a>
+      <a class="btn btn-outline-primary mx-2" type="submit" href="login.php">Login</a>
+      <a class="btn btn-outline-danger mx-2" type="submit" href="logout.php">Logout</a>
+    </form>
+
+
+  </div>
+</nav>
